891bb3171b5fcc404d4b07db6aa8e859542ac549
nghjakzz133-commits <nghjakzz133@gmail.com>
2026-01-09 23:17:00 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -193,3 +193,9 @@ if (hamburger && mobileMenu) {
     mobileMenu.classList.toggle('active');
   });
 }
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+});
